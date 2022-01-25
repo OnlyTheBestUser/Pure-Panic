03a79f3cfca6b9d0230056a7302b91d533fbb2c2
Michael Coviello <mcnika1234@gmail.com>
2022-01-25 12:49:16 +0000

    added jenkins build file

---
+++ JenkinsFile.java
@@ -0,0 +1,10 @@
+pipeline {
+    agent { docker { image 'maven:3.8.4-openjdk-11-slim' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'mvn --version'
+            }
+        }
+    }
+}
