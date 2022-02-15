4fc0e980875410581aee02911ad41a066305829e
Jack Bulson <jacktbulson@gmail.com>
2022-02-15 12:56:26 +0000

    Add debug methods to Renderer

--- CSC8503/GameTech/Renderer.cpp
+++ CSC8503/GameTech/Renderer.cpp
@@ -24,3 +24,106 @@ void Renderer::Update(float dt) {
 void Renderer::Render() {
 	rendererAPI->Render();
 }
+
+void Renderer::DrawString(const std::string& text, const Vector2& pos, const Vector4& colour, float size) {
+	DebugString s;
+	s.colour = colour;
+	s.pos = pos;
+	s.size = size;
+	s.text = text;
+	debugStrings.emplace_back(s);
+}
+
+void Renderer::DrawLine(const Vector3& start, const Vector3& end, const Vector4& colour) {
+	DebugLine l;
+	l.start = start;
+	l.end = end;
+	l.colour = colour;
+	debugLines.emplace_back(l);
+}
+
+void Renderer::DrawDebugData() {
+	/*if (debugStrings.empty() && debugLines.empty()) {
+		return; //don't mess with OGL state if there's no point!
+	}
+	BindShader(debugShader);
+
+	if (forceValidDebugState) {
+		glEnable(GL_BLEND);
+		glDisable(GL_DEPTH_TEST);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
+
+	int matLocation = glGetUniformLocation(debugShader->GetProgramID(), "viewProjMatrix");
+	Matrix4 pMat;
+
+	BindTextureToShader(font->GetTexture(), "mainTex", 0);
+
+	GLuint texSlot = glGetUniformLocation(boundShader->programID, "useTexture");
+
+	if (debugLines.size() > 0) {
+		pMat = SetupDebugLineMatrix();
+		glUniformMatrix4fv(matLocation, 1, false, pMat.array);
+		glUniform1i(texSlot, 0);
+		DrawDebugLines();
+	}
+
+	if (debugStrings.size() > 0) {
+		pMat = SetupDebugStringMatrix();
+		glUniformMatrix4fv(matLocation, 1, false, pMat.array);
+		glUniform1i(texSlot, 1);
+		DrawDebugStrings();
+	}
+
+	if (forceValidDebugState) {
+		glDisable(GL_BLEND);
+		glEnable(GL_DEPTH_TEST);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}*/
+}
+
+void Renderer::DrawDebugStrings() {
+	/*vector<Vector3> vertPos;
+	vector<Vector2> vertTex;
+	vector<Vector4> vertColours;
+
+	if (debugStrings.size() > 100) {
+		bool a = true;
+	}
+
+	for (DebugString& s : debugStrings) {
+		font->BuildVerticesForString(s.text, s.pos, s.colour, s.size, vertPos, vertTex, vertColours);
+	}
+
+	debugTextMesh->SetVertexPositions(vertPos);
+	debugTextMesh->SetVertexTextureCoords(vertTex);
+	debugTextMesh->SetVertexColours(vertColours);
+	debugTextMesh->UpdateGPUBuffers(0, vertPos.size());
+
+	BindMesh(debugTextMesh);
+	DrawBoundMesh();
+
+	debugStrings.clear();*/
+}
+
+void Renderer::DrawDebugLines() {
+	/*vector<Vector3> vertPos;
+	vector<Vector4> vertCol;
+
+	for (DebugLine& s : debugLines) {
+		vertPos.emplace_back(s.start);
+		vertPos.emplace_back(s.end);
+
+		vertCol.emplace_back(s.colour);
+		vertCol.emplace_back(s.colour);
+	}
+
+	debugLinesMesh->SetVertexPositions(vertPos);
+	debugLinesMesh->SetVertexColours(vertCol);
+	debugLinesMesh->UpdateGPUBuffers(0, vertPos.size());
+
+	BindMesh(debugLinesMesh);
+	DrawBoundMesh();
+
+	debugLines.clear();*/
+}
--- CSC8503/GameTech/Renderer.h
+++ CSC8503/GameTech/Renderer.h
@@ -14,8 +14,33 @@ namespace NCL {
 			void Render();
 
 			RendererAPI* GetRendererAPI() { return rendererAPI;}
+
+			// Render commands
+			void DrawString(const std::string& text, const Vector2& pos, const Vector4& colour = Vector4(0.75f, 0.75f, 0.75f, 1), float size = 20.0f);
+			void DrawLine(const Vector3& start, const Vector3& end, const Vector4& colour);
+			void DrawDebugData();
+			void DrawDebugLines();
+			void DrawDebugStrings();
 		protected:
 			RendererAPI* rendererAPI;
+		private:
+			struct DebugString {
+				Maths::Vector4 colour;
+				Maths::Vector2	pos;
+				float			size;
+				std::string		text;
+			};
+
+			struct DebugLine {
+				Maths::Vector3 start;
+				Maths::Vector3 end;
+				Maths::Vector4 colour;
+			};
+
+			SimpleFont* font;
+			std::vector<DebugString>	debugStrings;
+			std::vector<DebugLine>		debugLines;
+
 		};
 	}
 }
